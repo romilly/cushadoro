ec15090db9be19aada8ec8409291b41f25e16a4c
Romilly Cocking <romilly.cocking@gmail.com>
2016-01-03 17:19:11 +0000

    Added state test.

--- src/libraries/cushion/examples/CushionTest/CushionTest.ino
+++ src/libraries/cushion/examples/CushionTest/CushionTest.ino
@@ -15,7 +15,6 @@ MockHardware *hardware= new MockHardware;
 void test_beeps_for_one_sec_after_sitting_down() {
   ASSERT("timer one should not be enabled",!hardware->timerOneIsEnabled());
   cushion->handleEvent(SIT_DOWN);
-//  // hardware->dump();
   ASSERT("timer should tick in one sec",hardware->timerOneWillTickInOneSecond());
   ASSERT("led should be on",hardware->ledIsOn());
   cushion->handleEvent(TICK);
@@ -30,11 +29,28 @@ void test_waits_for_25_minutes_after_beeping() {
   ASSERT("sleep mode should be enabled",!hardware->isAsleep());
 }
 
+void test_stand_up_during_buzzing_returns_to_initial_state() {
+  cushion->handleEvent(SIT_DOWN);
+  cushion->handleEvent(GET_UP);
+  ASSERT("timer one should not be enabled",!hardware->timerOneIsEnabled());
+  ASSERT("led should be off",hardware->ledIsOff());
+}
+
+void test_stand_up_when_stitting_reverts_to_initial_state() {
+  cushion->handleEvent(SIT_DOWN);
+  cushion->handleEvent(TICK);
+  cushion->handleEvent(GET_UP);
+  ASSERT("timer one should not be enabled",!hardware->timerOneIsEnabled());
+  ASSERT("led should be off",hardware->ledIsOff());
+}
+
 void setup() {
   Serial.begin(9600);
   cushion = new Cushion(hardware);
   RUN(test_beeps_for_one_sec_after_sitting_down);
   RUN(test_waits_for_25_minutes_after_beeping);
+  RUN(test_stand_up_during_buzzing_returns_to_initial_state);
+  RUN(test_stand_up_when_stitting_reverts_to_initial_state);
   TEST_REPORT();
   Serial.println();
 }
