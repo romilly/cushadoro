c4e5b6169d8f28ac6a4479129af9509e08916ec3
Romilly Cocking <romilly.cocking@gmail.com>
2016-01-03 17:11:24 +0000

    Tidying up comments.

--- src/libraries/cushion/cushion_hardware.cpp
+++ src/libraries/cushion/cushion_hardware.cpp
@@ -27,40 +27,23 @@ void ArduinoHardware::disableTimer1() {
   interrupts();
 }
 
-// Set timer1_counter to the correct value for our interrupt interval
-  //timer1_counter = 64911;   // preload timer 65536-16MHz/256/100Hz
-  //timer1_counter = 64286;   // preload timer 65536-16MHz/256/50Hz
-  //timer1_counter = 34286;   // preload timer 65536-16MHz/256/2Hz
-
 void ArduinoHardware::loadTimer1(unsigned int count) {
   TCNT1 = count;
 }
 
-void ArduinoHardware::sleep()
-{
-  // Set sleep to full power down.  Only external interrupts or
-  // the watchdog timer can wake the CPU!
+void ArduinoHardware::sleep() {
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
   power_adc_disable();
   sleep_mode();
-
-  // CPU is now asleep and program execution completely halts!
-  // Once awake, execution will resume at this point.
-
   sleep_disable();
   power_all_enable();
 }
 
-void ArduinoHardware::enableWDTimer(){
+void ArduinoHardware::enableWDTimer() {
   noInterrupts();
-  // Set the watchdog reset bit in the MCU status register to 0.
   MCUSR &= ~(1<<WDRF);
-  // Set WDCE and WDE bits in the watchdog control register.
   WDTCSR |= (1<<WDCE) | (1<<WDE);
-  // TODO: find out how to set the timer for different intervals
-  // Set watchdog clock prescaler bits to a value of 8 seconds.
-  WDTCSR = (1<<WDP0) | (1<<WDP3);
-  // Enable watchdog as interrupt only (no reset).
+  WDTCSR = (1<<WDP0) | (1<<WDP3); // 8 sec
   WDTCSR |= (1<<WDIE);
   interrupts();
 }
@@ -68,16 +51,11 @@ void ArduinoHardware::enableWDTimer(){
 void ArduinoHardware::disableWDTimer() {
   noInterrupts();
   wdt_reset();
-  /* Clear WDRF in MCUSR */
   MCUSR &= ~(1<<WDRF);
-  /* Write logical one to WDCE and WDE */
-  /* Keep old prescaler setting to prevent unintentional time-out */
   WDTCSR |= (1<<WDCE) | (1<<WDE);
-  /* Turn off WDT */
   WDTCSR = 0x00;
   interrupts();
 }
-
 
 void ArduinoHardware::toggleLed() {
   CushionHardware::toggleLed();
